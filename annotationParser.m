4d9abd526d0ca90031886f38595c4d72674c4c97
Juan Guzman <jcguzma1@gmail.com>
2013-11-16 17:52:36 -0700

    Deleting!

--- annotationParser.m
+++
@@ -1,5 +0,0 @@
-xmlfile = fullfile('C:\Users\Mike Dude\Google Drive\PatternRec\PJ3\VOC2007\Annotations\009961.xml');
-xDoc = xmlread(xmlfile);
-filen = xDoc.getElementsByTagName('filename');
-thisElement = filen.item(0);
-found = char(thisElement.getFirstChild.getData);
